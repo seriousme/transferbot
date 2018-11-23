591d5182d999908573d373d929ec69de3d598742
Hans Klunder <hans.klunder@xs4all.nl>
2018-11-23 08:29:42 +0100

    fulfillment on V2 API

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,118 +1,118 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
-const staticData = {
+
+const { dialogflow } = require("actions-on-google");
+const app = dialogflow();
+
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+const db = firestore.collection("session");
+
+const localeData = {
   en: {
     currentAccount: "current account",
     savingsAccount: "savings account",
-    placeholder: "currently not available",
-    currency: ""
+    notAvailable: "currently not available",
+    currency: "dollar"
   },
   nl: {
     currentAccount: "betaalrekening",
     savingsAccount: "spaarrekening",
-    placeholder: "niet beschikbaar",
-    currency: " Euro"
+    notAvailable: "niet beschikbaar",
+    currency: "euro"
   }
 };
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-
 function getSessionData(sessionId) {
   return new Promise((resolve, reject) => {
-    admin
-      .database()
-      .ref("session/" + sessionId)
-      .once("value")
-      .then(snapshot => {
-        const data = snapshot.val();
-        if (data) {
+    db.doc(sessionId)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          const data = doc.data();
           resolve(data);
         } else {
-          const now = Number(new Date());
           const data = {
             currentAccountBalance: 1000,
             savingsAccountBalance: 0,
             transaction: {},
-            lastUpdate: now,
             sessionId: sessionId
           };
           resolve(data);
         }
+      })
+      .catch(err => {
+        console.log("getSessionData", err);
+        reject();
       });
   });
 }
 
-function bookTransAction(body) {
-  return new Promise((resolve, reject) => {
-    getSessionData(body.sessionId).then(session => {
-      const data = body.result;
-      const account = data.parameters.Account;
-      var amount = data.parameters.Amount;
-      if (typeof amount === "object") {
-        amount = data.parameters.Amount.amount;
-      }
+function bookTransAction(conv, params, languageCode) {
+  const sessionId = conv.body.session.replace(/.+\//, "");
+  const account = params.Account;
+  const amount = params.Amount.amount;
+  const { fulfillmentText } = conv.body.queryResult;
+  const locale = localeData[languageCode];
+  console.log(sessionId, account, amount, fulfillmentText, languageCode);
+  return getSessionData(sessionId)
+    .then(session => {
       session.currentAccountBalance -= amount;
-      if (account === staticData[body.lang].savingsAccount) {
+      if (account === locale.savingsAccount) {
         session.savingsAccountBalance += amount;
       }
-      data.lastUpdate = Number(new Date());
-      admin
-        .database()
-        .ref("session/" + body.sessionId)
-        .set(session);
-      resolve(session);
-    });
+      db.doc(sessionId)
+        .set(session)
+        .then(data => {
+          conv.data = { transferBot: data };
+          conv.ask(fulfillmentText);
+        });
+    })
+    .catch(err => console.log("bookTransAction", err));
+}
+
+function getBalance(conv, params, languageCode) {
+  const sessionId = conv.body.session.replace(/.+\//, "");
+  const account = params.Account;
+  const { fulfillmentText } = conv.body.queryResult;
+  const locale = localeData[languageCode];
+  console.log(sessionId, account, fulfillmentText, languageCode);
+  return getSessionData(sessionId).then(session => {
+    var balance = null;
+    if (account === locale.currentAccount) {
+      balance = session.currentAccountBalance;
+    }
+    if (account === locale.savingsAccount) {
+      balance = session.savingsAccountBalance;
+    }
+    if (balance !== null) {
+      conv.ask(
+        fulfillmentText.replace(
+          locale.notAvailable,
+          `${balance} ${locale.currency}`
+        )
+      );
+    } else {
+      conv.ask(fulfillmentText);
+    }
   });
 }
 
-function getBalance(body) {
-  return new Promise((resolve, reject) => {
-    getSessionData(body.sessionId).then(session => {
-      const data = body.result;
-      var balance = null;
-      if (data.parameters.Account === staticData[body.lang].currentAccount) {
-        balance = session.currentAccountBalance;
-      }
-      if (data.parameters.Account === staticData[body.lang].savingsAccount) {
-        balance = session.savingsAccountBalance;
-      }
-      if (balance !== null) {
-        resolve(balance);
-      } else {
-        reject();
-      }
-    });
-  });
-}
-
-exports.transferBot = functions.https.onRequest((request, response) => {
-  const body = request.body;
-  const result = body.result;
-  const action = result.action;
-
-  if (action === "account.transfer.confirm") {
-    bookTransAction(body).then(data => {
-      response.send({
-        speech: result.fulfillment.speech,
-        data: { transferBot: data }
-      });
-    });
-  }
-  if (action === "account.balance") {
-    getBalance(body)
-      .then(balance => {
-        const speech =
-          result.fulfillment.speech.replace(
-            staticData[body.lang].placeholder,
-            balance
-          ) + staticData[body.lang].currency;
-        response.send({ speech: speech });
-      })
-      .catch(balance => {
-        response.send(result.fulfillment);
-      });
-  }
+app.intent("Confirm transfer", (conv, params) => {
+  return bookTransAction(conv, params, "en");
 });
+
+app.intent("Bevestig overboeking", (conv, params) => {
+  return bookTransAction(conv, params, "nl");
+});
+
+app.intent("Account Balance", (conv, params) => {
+  return getBalance(conv, params, "en");
+});
+
+app.intent("Saldo", (conv, params) => {
+  return getBalance(conv, params, "nl");
+});
+
+exports.transferBot = functions.https.onRequest(app);
